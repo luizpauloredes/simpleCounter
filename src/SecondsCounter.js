8e04512f50d3947ded583f9b4c6ea66d2dd0ab67
Luiz Paulo <luizpaulo.redes@gmail.com>
2020-09-25 10:32:10 -0300

    counter running on

--- src/SecondsCounter.js
+++ src/SecondsCounter.js
@@ -1,26 +1,39 @@
 import React, { useEffect, useState } from 'react';
-import Proptypes from 'prop-types';
-import App from './App';
+
 
 //okay from here below
 const SecondsCounter = () => {
   const [timer, setTimer] = useState(0)
 
-  useEffect(() => {
+   useEffect(() => {
     setInterval(() => {
-      setTimer(timer => timer++)
+      setTimer(timer => timer + 1)     
     }, 1000)
   }, [])
 
+  let result = ('' + timer ).split('')
+  if (result.length === 1) {
+    return (
+      <div className="container py-3 bg-dark text-light">
+        <h1 >
+          <i className="far fa-clock mr-5 p"></i>000{result[0]}
+        </h1>
+      </div>      
+    )
 
-  return (
-    <div className="container py-3 bg-dark text-light">
-      <h1 >
-        <i className="far fa-clock mr-5 p"></i>
-      </h1>
+  } else if (result.length === 2) {
+    return (
+      <div className="container py-3 bg-dark text-light">
+        <h1 >
+          <i className="far fa-clock mr-5 p"></i>00{result[0]}{result[1]}
+        </h1>
+      </div>      
+    )
+  }
 
-    </div>
-  )
+
+
+  
 }
 
 
